6767d8359fad7343f3e987f4fcdb68b6745cc30e
jpmendes25 <joaopedromoliveira1996@gmail.com>
2021-03-04 01:12:20 -0300

    03/03/2021

---
+++ EXERCISES_JS/fundamentals/weak_typing.js
@@ -0,0 +1,12 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 2.2432
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//Evitar nomes genéricos e siglas
+let valor = ''
+let numero = 1
+let pqp = false // Produto Químico Perigoso (rsrsr)
