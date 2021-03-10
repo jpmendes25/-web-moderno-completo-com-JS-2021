8fc1a9d8ec9353e683527d97c95bcc5e119a25ae
jpmendes25 <joaopedromoliveira1996@gmail.com>
2021-03-10 19:51:42 -0300

    10/03/2021

---
+++ JavaScript/Examples_JS/object/OO.js
@@ -0,0 +1,22 @@
+// CÓDIGO NÃO EXECUTÁVEL!!!
+
+// Procedual
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() // O foco passou a ser o objeto
+
+// Principios importantes:
+// 1. abstração
+// 2. encapsulamento
+// 3. herança (prototype)
+// 4. polimorfismo
