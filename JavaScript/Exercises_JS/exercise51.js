1811adbe5ada8e504a468d3ecc3a5eefb04e3251
jpmendes25 <joaopedromoliveira1996@gmail.com>
2021-03-14 14:38:45 -0300

    14/03/2021

---
+++ JavaScript/Exercises_JS/exercise51.js
@@ -0,0 +1,15 @@
+// Crie uma função que receba uma array e retorne o primeiro e o último elemento desse array como um novo array:
+
+// Exemplos:
+// receberPrimeiroEUltimoElemento([7,14,"olá"]) // retornará [7, "olá"]
+// receberPrimeiroEUltimoElemento([-100, "aplicativo", 16]) // retornará [-100, 16]
+
+receberPrimeiroEUltimoElemento = (arrayRecebido) => {
+    
+}
+
+/*
+
+GABARITO
+
+*/
